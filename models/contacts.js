e7e832af58ac6908abd33a5b121e6e0e381e13fe
Piligrim41m <lightsun41m@gmail.com>
2023-03-03 19:36:13 +0200

    setting up project routes

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,94 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises
+const path = require('path')
+const shortid = require('shortid')
 
-const listContacts = async () => {}
+const contactsPath = path.join("models", "contacts.json")
 
-const getContactById = async (contactId) => {}
+async function readData() {
+  try {
+    const contacts = await fs.readFile(contactsPath)
+  return JSON.parse(contacts)
+  } catch (err) {
+    console.log(err.message)
+  }
+};
 
-const removeContact = async (contactId) => {}
+async function writeData(data) {
+  const stringifiedData = JSON.stringify(data) 
+  try {
+    await fs.writeFile(contactsPath, stringifiedData)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  try {
+    const contacts = await readData();
+    return contacts;
+  } catch (err) {
+    console.log(err.message)
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const allContacts = await readData()
+    const [contact] = allContacts.filter(contact => contact.id === contactId);
+    return contact;
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+const addContact = async (body) => { 
+  const { name, email, phone } = body;
+  const newContact = {
+    id: shortid.generate(),
+    name,
+    email,
+    phone,
+  }
+  try {
+    const allContacts = await readData();
+    const upgradContacts = [...allContacts, newContact];
+    await writeData(upgradContacts)
+    return newContact
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+const removeContact = async (contactId) => {
+  try {
+    const allContacts = await readData();
+    const updatedContacts = allContacts.filter(contact => contact.id !== contactId);
+    await writeData(updatedContacts)
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+    try {
+    const allContacts = await readData();
+    const updatedContact = allContacts.map(contact => {
+      if (contactId === contact.id) {
+        contact = {
+          id: contactId,
+          name,
+          email,
+          phone,
+        };
+      }
+      return contact;
+    })
+      await writeData(updatedContact)
+  } catch (err) {
+    console.log(err.message)
+  }
+  }
 
 module.exports = {
   listContacts,
