39f8323337f88fd73c49126a569cdf7557838a3e
Piligrim41m <lightsun41m@gmail.com>
2023-03-11 20:36:59 +0200

    server connection setup

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+const mongoose =require('mongoose')
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+// const DB_HOST = "mongodb+srv://lightsun41m:DsfzsewFveBasPCY@cluster0.gydekh5.mongodb.net/db-contacts?retryWrites=true&w=majority"
+const { DB_HOST } = process.env
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    console.log('Database connection sucessful')
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    })
+  })
+  .catch(err => {
+    console.error(err.message);
+    process.exit(1);
 })
