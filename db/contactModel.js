7eafc447fd58c00e0c2985c66673762e176b7ed0
Piligrim41m <lightsun41m@gmail.com>
2023-03-16 20:34:32 +0200

    Update contactModel.js

--- db/contactModel.js
+++ db/contactModel.js
@@ -1,4 +1,4 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model, SchemaTypes} = require('mongoose');
 
 const contactSchema = new Schema({
     name: {
@@ -15,8 +15,12 @@ const contactSchema = new Schema({
         type: Boolean,
         default: false,
     },
-})
+    owner: {
+        type: SchemaTypes.ObjectId,
+        ref: 'user',
+    },
+});
 
-const Contact = model('contacn', contactSchema);
+const Contact = model('contact', contactSchema);
 
 module.exports = Contact;
