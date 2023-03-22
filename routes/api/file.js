aa2059a38c37310e74c84bf173ff20e87475d3ad
Piligrim41m <lightsun41m@gmail.com>
2023-03-22 17:51:33 +0200

    Add routes for users avatar

---
+++ routes/api/file.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path')
+
+const router = express.Router();
+
+const PICTURE_PATH = path.resolve('./public/avatars');
+
+router.get('/:fileName', express.static(PICTURE_PATH));
+
+module.exports = router;
